af612f763e89164035aa6f11767fb374d31bb0e4
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-11-01 19:37:55 +0530

    subha 01-11-2022

--- resources/views/web/teacher.blade.php
+++ resources/views/web/teacher.blade.php
@@ -1,6 +1,9 @@
 @extends('web.layout')
 @section('content')
     <div class="tab-content dashboard-tab-content" id="myTabContent">
-        Teachers
-    </div>
+        <div class="second-sec assignment-section-col">
+            <div class="teacher-page-sec">
+                <h2>Teachers</h2>                        
+            </div>
+        </div>
 @endsection
