3a413c2d6683c5a2417ccb60919c27795b486973
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-11-02 20:06:59 +0530

    subha 02-11-2022

--- resources/views/web/common/header.blade.php
+++ resources/views/web/common/header.blade.php
@@ -1,5 +1,19 @@
 <div class="container-fluid dashboard-tab-section">
     <!-- <div class="container"> -->
+
+    <!-------HEADER LOGO----------->
+
+    <div class="header-user-profile-sec">
+        <div class="header-logo">
+            <img src="{{ asset('web/images/login-page-img.jpg') }}" alt="">
+            <span>Bbeducation</span>
+        </div>
+        <div class="user-name-sec">
+        <i class="fa-solid fa-user"></i><span>Lorem Ipsum</span>
+        </div>
+    </div>
+
+
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item @if ($headerTitle == 'Dashboard') header-active @endif ">
             <a class="nav-link" href="{{ URL::to('/dashboard') }}" ><i class="fa-solid fa-house-chimney">Home</i></a>
@@ -24,9 +38,12 @@
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa-solid fa-gear">Settings</i></a>
                 <div class="dropdown-menu">
                 <a class="dropdown-item" href="{{ URL::to('/profile') }}">Profile</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+                <!-- <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a> -->
                 </div>
+        </li>
+        <li class="nav-item @if ($headerTitle == 'Logout') header-active @endif ">
+            <a class="nav-link" href="{{ URL::to('/logout') }}"><i class="fa-solid fa-person">Logout</i></a>
         </li>
     </ul>
 
