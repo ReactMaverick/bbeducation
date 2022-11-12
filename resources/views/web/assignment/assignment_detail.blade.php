13befe1cf1093c2fdbc1034f8514f434a3724e65
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-11-12 16:59:21 +0530

    subha3 12-11-2022

--- resources/views/web/assignment/assignment_detail.blade.php
+++ resources/views/web/assignment/assignment_detail.blade.php
@@ -387,7 +387,77 @@
 
             <div class="button-section">
                 <button class="button-1">Candidate Vetting</button>
-                <button class="button-2">Block Booking</button>
+                <!-- <button class="button-2">Block Booking -->
+
+                <button type="button" class="btn btn-primary button-2" data-toggle="modal" data-target="#myModal">
+                    Block Booking
+                </button>
+
+                <!-- The Modal -->
+                <div class="modal fade" id="myModal">
+                    <div class="modal-dialog modal-dialog-centered calendar-modal-section">
+                        <div class="modal-content calendar-modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header calendar-modal-header">
+                                <h4 class="modal-title">Edit Working Day</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <div class="calendar-heading-sec">
+                                <i class="fa-solid fa-pencil school-edit-icon"></i>
+                                <h2>Edit Assignment Day</h2>
+                            </div>
+
+                            <div class="modal-input-field-section">
+                                <div class="modal-input-field">
+                                    <label class="form-check-label">Date</label>
+                                    <input type="date" class="form-control" name="booked_day" id="booked_day" value="">
+                                </div>
+
+                                <div class="form-group calendar-form-filter">
+                                    <label for="inputState">Part of Day</label>
+                                    <select id="inputState" class="form-control">
+                                        <option selected>Choose...</option>
+                                        <option>...</option>
+                                    </select>
+                                </div>
+
+                                <div class="modal-input-field">
+                                    <label class="form-check-label">Percentage of a day</label>
+                                    <input type="number" class="form-control" name="booked_day" id="booked_day" value="">
+                                </div>
+
+                                <div class="modal-input-field">
+                                    <label class="form-check-label">Hours</label>
+                                    <input type="time" class="form-control" name="booked_day" id="booked_day" value="">
+                                </div>
+
+                                <div class="modal-side-field">
+                                <label class="form-check-label">Charge</label>
+                                    <input type="text" class="form-control" name="booked_day" id="booked_day" value="">
+                                </div>
+                                <div class="modal-side-field second">
+                                <label class="form-check-label">Pay</label>
+                                    <input type="text" class="form-control" name="booked_day" id="booked_day" value="">
+                                </div>
+                            </div>
+
+
+                            <!-- Modal body
+                            <div class="modal-body">
+                                Modal body..
+                            </div> -->
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer calendar-modal-footer">
+                                <button type="submit" class="btn btn-secondary" data-dismiss="modal">Submit</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- </button> -->
                 <button type="submit" class="btn btn-primary button-3">Submit</button>
             </div>
 
