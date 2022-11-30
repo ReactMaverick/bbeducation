8b512b0b3adde4dea8651f87825b2c95cd9615c8
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-11-30 17:46:48 +0530

    subha2 30-11-2022

--- resources/views/web/finance/finance_timesheet.blade.php
+++ resources/views/web/finance/finance_timesheet.blade.php
@@ -1,4 +1,247 @@
 @extends('web.layout')
 @section('content')
-Finance timesheets
+<div class="assignment-detail-page-section">
+    <div class="row assignment-detail-row">
+
+        @include('web.teacher.teacher_sidebar')
+
+        <div class="col-md-10 topbar-sec">
+
+            <div class="topbar-Section">
+                <i class="fa-solid fa-address-book">
+                    <span class="topbar-text">Has Perm, Position</span>
+                </i>
+                <i class="fa-solid fa-users">
+                    <span class="topbar-text">Secondary</span>
+                </i>
+                <i class="fa-brands fa-black-tie">
+                    <span class="topbar-text">Teacher</span>
+                </i>
+                <i class="fa-solid fa-star topbar-star-icon"></i>
+
+                <i class="fa-regular fa-calendar-days">
+                    <span class="topbar-text">Calendar</span>
+                </i>
+
+                <a href="#">
+                    <i class="fa-solid fa-trash trash-icon"></i>
+                </a>
+            </div>
+
+            <div class="school-detail-right-sec">
+                <div class="school-details-first-sec">
+                    <div class="details-heading">
+                        <h2>Profession</h2>
+                        <a data-toggle="modal" data-target="#DetailModal" style="cursor: pointer;"><i
+                                class="fa-solid fa-pencil"></i></a>
+                    </div>
+
+                    <div class="about-school-section">
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Candidate Type</h2>
+                            </div>
+                            <div class="teacher-profession-name-text">
+                                <p>Teacher</p>
+                            </div>
+                        </div>
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>SubType</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>Mr</p>
+                            </div> -->
+                        </div>
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Age Range</h2>
+                            </div>
+                            <div class="grid-refs-sec">
+                                <div class="grid-refs-text1">
+                                    <p>Secondary</p>
+                                </div>
+
+                                <!-- <div class="grid-refs-text1">
+                                    <p>Alonso</p>
+                                </div> -->
+
+                            </div>
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <label for="vehicle1">NQT Required</label>
+                            </div>
+                            <div class="teacher-profession-heading-text">
+                                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                            </div>
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>NQT Completed</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>abc</p>
+                            </div> -->
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>TRN</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>12-04-1992</p>
+                            </div> -->
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Imported Title</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>Spanish</p>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+
+                <div class="school-details-second-sec">
+                    <div>
+                        <div class="details-heading">
+                            <h2>Interview</h2>
+                            <a data-toggle="modal" data-target="#DetailModal" style="cursor: pointer;"><i
+                                    class="fa-solid fa-pencil"></i></a>
+                        </div>
+
+                        <!-- <div class="about-school-section"> -->
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Notes</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-headnameing-text">
+                                <p>Flat 3, 14 Cholmeley Park</p>
+                                <p>N6 5EU</p>
+                            </div> -->
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Interview Quality</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>51.5742105</p>
+                                <p>-0.1434667</p>
+                            </div> -->
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>Language Skills</h2>
+                            </div>
+                            <!-- <div class="teacher-profession-name-text">
+                                <p>51.5742105</p>
+                                <p>-0.1434667</p>
+                            </div> -->
+                        </div>
+
+                        <div class="school-name-section">
+                            <div class="teacher-profession-heading-text">
+                                <h2>interview Details</h2>
+                            </div>
+                            <div class="teacher-profession-name-text">
+                                <p>Interviewed by Georgia Symeou on 24-05-2016</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="school-detail-right-sec">
+
+                <div class="school-details-contact-second-sec">
+                    <div class="contact-heading">
+                        <div class="contact-heading-text">
+                            <h2>Teaching Subjects</h2>
+                        </div>
+                        <div class="contact-icon-sec">
+                            <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a style="cursor: pointer;" class="disabled-link" id="editContactBttn">
+                                <i class="fa-solid fa-pencil school-edit-icon"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="assignment-finance-table-section">
+                        <table class="table school-detail-page-table" id="myTable">
+                            <thead>
+                                <tr class="school-detail-table-heading">
+                                    <th>Subject</th>
+                                    <th>Main</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-body-sec">
+                                <tr class="school-detail-table-data editContactRow">
+                                    <td></td>
+                                    <td>Y</td>
+                                </tr>
+                                <tr class="school-detail-table-data editContactRow">
+                                    <td></td>
+                                    <td>N</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+                <div class="school-details-contact-second-sec">
+                    <div class="contact-heading">
+                        <div class="contact-heading-text">
+                            <h2>Qualifications</h2>
+                        </div>
+                        <div class="contact-icon-sec">
+                            <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a style="cursor: pointer;" class="disabled-link" id="editContactBttn">
+                                <i class="fa-solid fa-pencil school-edit-icon"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="assignment-finance-table-section">
+                        <table class="table school-detail-page-table" id="myTable">
+                            <thead>
+                                <tr class="school-detail-table-heading">
+                                    <th>Type</th>
+                                    <th>Qualification</th>
+                                    <th>QTS</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-body-sec">
+                                <tr class="school-detail-table-data editContactRow">
+                                    <td>BA</td>
+                                    <td>Primary Education</td>
+                                    <td>N</td>
+                                </tr>
+                                <tr class="school-detail-table-data editContactRow">
+                                    <td>MA</td>
+                                    <td>Physical Activity, Health & Sports Training</td>
+                                    <td>Y</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
