b9732179952dc10a81399e002016fab925b45e03
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-11-30 18:16:39 +0530

    subha school assignment 30-11-2022

--- resources/views/web/school/school_assignment.blade.php
+++ resources/views/web/school/school_assignment.blade.php
@@ -1,67 +1,80 @@
 @extends('web.layout')
 @section('content')
-    <div class="assignment-detail-page-section">
-        <div class="row assignment-detail-row">
+<div class="assignment-detail-page-section">
+    <div class="row assignment-detail-row">
 
-            @include('web.school.school_sidebar')
+        @include('web.school.school_sidebar')
 
-            <div class="col-md-10 topbar-sec">
+        <div class="col-md-10 topbar-sec">
 
-                @include('web.school.school_header')
+            @include('web.school.school_header')
 
-                <div class="school-assignment-sec">
+            <div class="school-assignment-sec">
 
 
-                    <div class="school-assignment-section">
-                        <div class="school-assignment-contact-heading-text">
-                            <h2>Assignments</h2>
-                        </div>
-                        <div class="school-assignment-contact-heading">
-                            <div class="form-check paid-check">
-                                <label for="includeAll">Include All</label>
-                                <input type="checkbox" id="includeAll" name="include" value="1"
-                                    @if (app('request')->input('include') == '1') checked @endif>
+                <div class="school-assignment-section">
+                    <div class="school-assignment-contact-heading-text">
+                        <h2>Assignments</h2>
+                    </div>
+                    <div class="school-assignment-contact-heading">
+                        <div class="school-assignment-contact-search">
+                            <div class="form-check paid-check school-assignment-paid-check">
+                                <div class="school-assignment-contact-checkbox">
+                                    <label for="includeAll">Include All</label>
+                                    <input type="checkbox" id="includeAll" name="include" value="1" <?php
+                                        (app('request')->input('include') == '1')?'checked':'' ?> >
+                                </div>
 
-                                <label>Status</label>
-                                <select id="assignmentStatus" class="form-control">
-                                    <option value="">Choose One</option>
-                                    @foreach ($statusList as $key1 => $status)
-                                        <option value="{{ $status->description_int }}"
-                                            @if (app('request')->input('status') == $status->description_int) selected @endif>
-                                            {{ $status->description_txt }}
-                                        </option>
-                                    @endforeach
-                                </select>
+                                <div class="school-assignment-contact-select-field">
+                                    <div class="school-assignment-contact-select-label">
+                                        <label>Status</label>
+                                    </div>
+                                    <div class="school-assignment-contact-select-option">
+                                        <select id="assignmentStatus" class="form-control">
+                                            <option value="">Choose One</option>
+                                            @foreach ($statusList as $key1 => $status)
+                                            <option value="{{ $status->description_int }}" <?php (app('request')->
+                                                input('status')
+                                                == $status->description_int)?'selected':'' ?>>
+                                                {{ $status->description_txt }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
                             </div>
-
-                            <div class="school-assignment-contact-icon-sec">
-                                <a href="javascript:void(0)"><i class="fa-solid fa-xmark"></i></a>
-                                <a href="javascript:void(0)"><i class="fa-solid fa-plus"></i></a>
-                                <a href="javascript:void(0)"><i class="fa-solid fa-pencil school-edit-icon"></i></a>
-                            </div>
                         </div>
 
 
+                        <div class="school-assignment-contact-icon-sec">
+                            <a href="javascript:void(0)"><i class="fa-solid fa-xmark"></i></a>
+                            <a href="javascript:void(0)"><i class="fa-solid fa-plus"></i></a>
+                            <a href="javascript:void(0)"><i class="fa-solid fa-pencil school-edit-icon"></i></a>
+                        </div>
+                    </div>
 
-                        <table class="table school-detail-page-table" id="myTable">
-                            <thead>
-                                <tr class="school-detail-table-heading">
-                                    <th>Year Group</th>
-                                    <th>Status</th>
-                                    <th>Type</th>
-                                    <th>Days</th>
-                                    <th>Teacher</th>
-                                    <th>Date</th>
-                                    <th>Weekday</th>
-                                </tr>
-                            </thead>
-                            <tbody class="table-body-sec">
-                                <?php $completeCount = 0; ?>
-                                @foreach ($assignmentList as $key => $Assignment)
-                                    @if ($Assignment->assignmentStatus == 'Completed')
-                                        <?php $completeCount += 1; ?>
-                                    @endif
-                                    <?php
+
+
+                    <table class="table school-detail-page-table" id="myTable">
+                        <thead>
+                            <tr class="school-detail-table-heading">
+                                <th>Year Group</th>
+                                <th>Status</th>
+                                <th>Type</th>
+                                <th>Days</th>
+                                <th>Teacher</th>
+                                <th>Date</th>
+                                <th>Weekday</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-body-sec">
+                            <?php $completeCount = 0; ?>
+                            @foreach ($assignmentList as $key => $Assignment)
+                            @if ($Assignment->assignmentStatus == 'Completed')
+                            <?php $completeCount += 1; ?>
+                            @endif
+                            <?php
                                     $yDescription = '';
                                     if (in_array($Assignment->ageRange_int, ['1', '2', '3', '4'])) {
                                         if ($Assignment->yearGroup_int != null) {
@@ -84,78 +97,78 @@
                                     }
                                     
                                     ?>
-                                    <tr class="table-data" onclick="assignmentDetail()">
-                                        <td>
-                                            @if ($yDescription == null || $yDescription == '')
-                                                {{ $Assignment->yearGroup }}
-                                            @else
-                                                {{ $yDescription }}
-                                            @endif
-                                        </td>
-                                        <td>{{ $Assignment->assignmentStatus }}</td>
-                                        <td>{{ $Assignment->assignmentType }}</td>
-                                        <td>{{ $Assignment->days_dec }} {{ $Assignment->type_txt }}</td>
-                                        <td>
-                                            @if ($Assignment->techerFirstname || $Assignment->techerSurname)
-                                                {{ $Assignment->techerFirstname }} {{ $Assignment->techerSurname }}
-                                            @else
-                                                No teacher
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($Assignment->firstDate_dte)
-                                                {{ date('d-m-Y', strtotime($Assignment->firstDate_dte)) }}
-                                            @else
-                                                {{ date('d-m-Y', strtotime($Assignment->timestamp_ts)) }}
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($Assignment->firstDate_dte)
-                                                {{ date('l', strtotime($Assignment->firstDate_dte)) }}
-                                            @else
-                                                {{ date('l', strtotime($Assignment->timestamp_ts)) }}
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                            <tr class="table-data" onclick="assignmentDetail()">
+                                <td>
+                                    @if ($yDescription == null || $yDescription == '')
+                                    {{ $Assignment->yearGroup }}
+                                    @else
+                                    {{ $yDescription }}
+                                    @endif
+                                </td>
+                                <td>{{ $Assignment->assignmentStatus }}</td>
+                                <td>{{ $Assignment->assignmentType }}</td>
+                                <td>{{ $Assignment->days_dec }} {{ $Assignment->type_txt }}</td>
+                                <td>
+                                    @if ($Assignment->techerFirstname || $Assignment->techerSurname)
+                                    {{ $Assignment->techerFirstname }} {{ $Assignment->techerSurname }}
+                                    @else
+                                    No teacher
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($Assignment->firstDate_dte)
+                                    {{ date('d-m-Y', strtotime($Assignment->firstDate_dte)) }}
+                                    @else
+                                    {{ date('d-m-Y', strtotime($Assignment->timestamp_ts)) }}
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($Assignment->firstDate_dte)
+                                    {{ date('l', strtotime($Assignment->firstDate_dte)) }}
+                                    @else
+                                    {{ date('l', strtotime($Assignment->timestamp_ts)) }}
+                                    @endif
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
 
-                    </div>
-                    <div class="assignment-first-sec">
-                        <div class="assignment-left-sidebar-section">
-                            <div class="school-assignment-sidebar-sec">
-                                <div class="assignment-sidebar-data">
-                                    <h2>{{ $completeCount }}</h2>
-                                </div>
-                                <div class="school-assignment-sidebar-sec-text">
-                                    <span>Completed Assignments</span>
-                                </div>
+                </div>
+                <div class="assignment-first-sec">
+                    <div class="assignment-left-sidebar-section">
+                        <div class="school-assignment-sidebar-sec">
+                            <div class="assignment-sidebar-data">
+                                <h2>{{ $completeCount }}</h2>
                             </div>
-                            <div class="school-assignment-sidebar-sec">
-                                <div class="assignment-sidebar-data2">
-                                    <h2>{{ $completeCount }}</h2>
-                                </div>
-                                <div class="school-assignment-sidebar-sec-text">
-                                    <span>Total Days</span>
-                                </div>
+                            <div class="school-assignment-sidebar-sec-text">
+                                <span>Completed Assignments</span>
                             </div>
-
                         </div>
+                        <div class="school-assignment-sidebar-sec">
+                            <div class="assignment-sidebar-data2">
+                                <h2>{{ $completeCount }}</h2>
+                            </div>
+                            <div class="school-assignment-sidebar-sec-text">
+                                <span>Total Days</span>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <script>
-        $(document).ready(function() {
-            $('#myTable').DataTable();
-        });
+<script>
+$(document).ready(function() {
+    $('#myTable').DataTable();
+});
 
-        function assignmentDetail() {
-            window.location.href = "{{ URL::to('/assignment-details') }}";
-        }
-    </script>
+function assignmentDetail() {
+    window.location.href = "{{ URL::to('/assignment-details') }}";
+}
+</script>
 @endsection
