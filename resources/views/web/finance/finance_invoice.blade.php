41ee5be5562ae168b60788d938ac64e6a0be9a56
amitWebsadroit <107679138+amitWebsadroit@users.noreply.github.com>
2022-12-01 20:07:33 +0530

    subha finance 01-12-2022

--- resources/views/web/finance/finance_invoice.blade.php
+++ resources/views/web/finance/finance_invoice.blade.php
@@ -1,4 +1,166 @@
 @extends('web.layout')
 @section('content')
-Finance invoice
+<div class="finance-invoice-right-sec">
+
+    <div class="finance-invoice-contact-first-sec">
+        <div class="invoice-top-section">
+            <div class="form-group invoice-top-input-sec">
+                <label for="staticEmail" class="col-form-label">Timesheet-Until:</label>
+                <input type="date" id="staticEmail" placeholder="25-11-2022" value="">
+            </div>
+            <div class="invoice-top-btn-sec">
+                <button>Select None</button>
+            </div>
+
+            <div class="invoice-top-btn-sec">
+                <button>Select All</button>
+            </div>
+            <div class="invoice-edit-icon">
+                <a data-toggle="modal" data-target="#DetailModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+            </div>
+        </div>
+        <div class="finance-invoice-table-section">
+            <table class="table finance-timesheet-page-table" id="myTable">
+                <thead>
+                    <tr class="school-detail-table-heading">
+                        <th>School</th>
+                        <th>Teacher</th>
+                        <th>Date</th>
+                        <th>Charge</th>
+                        <th>Pay</th>
+                    </tr>
+                </thead>
+                <tbody class="table-body-sec">
+                    <tr class="school-detail-table-data editContactRow">
+                        <td>Acland Burghley School</td>
+                        <td>Vivian Amoako</td>
+                        <td>19-09-2022</td>
+                        <td>117</td>
+                        <td>74.52</td>
+                    </tr>
+                    <tr class="school-detail-table-data editContactRow">
+                        <td>Acland Burghley School</td>
+                        <td>Vivian Amoako</td>
+                        <td>19-09-2022</td>
+                        <td>117</td>
+                        <td>74.52</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="invoice-process-btn">
+            <button>Process Selected</button>
+        </div>
+    </div>
+
+
+    <div class="finance-invoice-contact-second-sec">
+
+        <div class="invoice-top-second-section">
+            <div class="form-group invoice-top-first-input-sec">
+                <label for="staticEmail" class="col-form-label">Invoice From</label>
+                <input type="date" id="staticEmail" placeholder="25-11-2022" value="">
+            </div>
+            <div class="form-group invoice-top-second-input-sec">
+                <label for="staticEmail" class="col-form-label">to</label>
+                <input type="date" id="staticEmail" placeholder="25-11-2022" value="">
+            </div>
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-arrows-rotate"></i>
+                </a>
+            </div>
+            <div class="invoice-checkbox-top-section">
+                <div class="invoice-checkbox-sec">
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                </div>
+                <div class="invoice-checkbox-sec">
+                    <label for="vehicle1">Show Sent</label>
+                </div>
+            </div>
+
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-file-lines"></i>
+                </a>
+            </div>
+
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-arrow-right-arrow-left"></i>
+                </a>
+            </div>
+
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </a>
+            </div>
+
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-envelope"></i>
+                    <div class="finance-invoice-second-icon-sec">
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                </a>
+            </div>
+
+            <div class="finance-invoice-icon-sec">
+                <a data-toggle="modal" data-target="#ContactItemAddModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-envelope"></i>
+                </a>
+            </div>
+            <div class="invoice-second-edit-icon">
+                <a data-toggle="modal" data-target="#DetailModal" style="cursor: pointer;">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+            </div>
+        </div>
+        <div class="finance-invoice-table-section">
+            <table class="table finance-timesheet-page-table" id="myTable">
+                <thead>
+                    <tr class="school-detail-table-heading">
+                        <th>Invoice ID</th>
+                        <th>Date</th>
+                        <th>School</th>
+                        <th>Gross</th>
+                        <th>Net</th>
+                        <th>Days</th>
+                        <th>Tch</th>
+                        <th>Email</th>
+                        <th>Factor</th>
+                    </tr>
+                </thead>
+                <tbody class="table-body-sec">
+                    <tr class="school-detail-table-data editContactRow">
+                        <td>Acland Burghley School</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                    </tr>
+                    <tr class="school-detail-table-data editContactRow">
+                        <td>Acland Burghley School</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                        <td>432</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
 @endsection
