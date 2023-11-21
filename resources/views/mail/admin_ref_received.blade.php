8045d6f6110ef19f6ccf77f2e66a2b9b34c64a5d
Barun Karmakar <kumarbarun137@gmail.com>
2023-11-21 14:34:20 +0530

    sanjoy 1st 21-11-2023

--- resources/views/mail/admin_ref_received.blade.php
+++ resources/views/mail/admin_ref_received.blade.php
@@ -60,8 +60,10 @@
                 <td style="padding: 10px 10px;border-top: 1px solid #dedede;">
                     <h3 style="margin-bottom: 10px;">*PLEASE DO NOT RESPOND TO THIS EMAIL, THIS EMAIL ADDRESS IS NOT
                         MONITORED.*</h3>
-                    <p>For any queries, please email <a href="mailto:Saleem@bbe-edu.co.uk"
-                            target="_blank">Saleem@bbe-edu.co.uk</a></p>
+                    <p>For any queries, please email <a
+                            href="mailto:{{ $mailData['companyDetail'] ? $mailData['companyDetail']->company_name : '' }}"
+                            target="_blank">{{ $mailData['companyDetail'] ? $mailData['companyDetail']->company_name : '' }}</a>
+                    </p>
                 </td>
             </tr>
             <tr>
